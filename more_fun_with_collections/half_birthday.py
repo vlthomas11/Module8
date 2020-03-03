3e64af80118318aaeb181479b1640a0a8acdf9eb
VLThomas11 <vthomas2@DMACC.edu>
2020-03-03 16:01:37 -0600

    update

--- more_fun_with_collections/half_birthday.py
+++ more_fun_with_collections/half_birthday.py
@@ -1,5 +1,11 @@
-import datetime
+from datetime import datetime, timedelta
 
 
-def half_birthday():
-    pass
+def half_birthday(dob):
+    dob_date = datetime.strptime(dob, '%m/%d/%Y')
+    half_dob = dob_date + timedelta(days=183)
+    return half_dob
+
+
+if __name__ == '__main__':
+    half_birthday('12/01/2019')
--- test/test_half_birthday.py
+++ test/test_half_birthday.py
@@ -1,10 +1,13 @@
 import unittest
 
+from datetime import datetime, timedelta
 from more_fun_with_collections.half_birthday import half_birthday as topic4
+
 
 class MyTestCase(unittest.TestCase):
     def test_half_brithday(self):
-        self.assertEqual(topic4('2019/12/01'), '2020/06/01')
+        self.assertEqual(topic4('12/01/2019'), datetime.strptime('06/01/2020', '%m/%d/%Y'))
+
 
 if __name__ == '__main__':
     unittest.main()
